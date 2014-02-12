2bd21c5ae5c333b22be5a5cd6f1b49dd0a1948c7
Tomás Pica <tomas.pica@gmail.com>
2014-02-12 23:10:00 +0000

    N-fold Iris Classification

---
+++ knn/iris_nfold.py
@@ -0,0 +1,80 @@
+from pprint import pprint
+
+# read more: http://www.numpy.org/
+import numpy
+
+# read more: http://pandas.pydata.org/
+import pandas
+
+# read more: http://wiki.scipy.org/PyLab
+import pylab  # uncomment for plots
+
+# read more: http://scikit-learn.org/stable/
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def train_k_neighbours(n, features_set, predict_set):
+    model = KNeighborsClassifier(n_neighbors=n)
+    # http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+    model.fit(features_set, predict_set)
+    return model
+
+
+def split_set(dataframe, pct=0.3):
+    uniform_array = numpy.random.uniform(0, 1, len(dataframe))
+    train_set = dataframe[uniform_array > pct]
+    test_set = dataframe[uniform_array <= pct]
+    return train_set, test_set
+
+
+def n_fold_sets(n, dataframe):
+    df_len = len(dataframe)
+    # disordered whole dataframe
+    d_df = numpy.random.choice(dataframe.index.values, df_len)
+    steps = range(0, df_len + n, df_len/n)
+    sets = {}
+    for i in range(0, n):
+        sets[i] = dataframe.ix[d_df[steps[i]:steps[i+1]]]
+    return sets
+
+csv = "iris.csv"
+
+data_frame = pandas.read_csv(csv)
+# >>> data_frame.info()
+# <class 'pandas.core.frame.DataFrame'>
+# Int64Index: 150 entries, 0 to 149
+#
+# http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html
+
+features = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width']
+predict = 'class'
+
+sets = n_fold_sets(10, data_frame)
+
+results = []
+for n in range(1, 100):
+    accuracy = []
+    for i in range(1, 10):
+        test_set = sets[i]
+        train_set = pandas.concat([sets[s] for s in sets if s != i])
+
+        model = train_k_neighbours(n, train_set[features], train_set[predict])
+
+        preds = model.predict(test_set[features])
+
+        accuracy.append(
+            numpy.where(
+                preds == test_set[predict], 1, 0
+            ).sum() / float(len(test_set))
+        )
+
+    # average accuracy for n
+    results.append([n, sum(accuracy)/10.0])
+
+pprint(results)
+
+# uncomment for plots
+results = pandas.DataFrame(results, columns=["n", "accuracy"])
+pylab.plot(results.n, results.accuracy)
+pylab.title("Accuracy with Increasing K")
+pylab.show()
